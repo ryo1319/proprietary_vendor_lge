ca810237d2bde239ac7710c8b3e520f2cf91b2a6
ma34s <ma34s.sss@gmail.com>
2014-04-26 14:22:46 +0900

    lo01f /update binaries
    Change-Id: I052de4eaa26ea98fc743232bf12ac2c0a0d0dee4

--- l01f/l01f-vendor-blobs.mk
+++ l01f/l01f-vendor-blobs.mk
@@ -129,6 +129,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/l01f/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
     vendor/lge/l01f/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/lge/l01f/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
+    vendor/lge/l01f/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     vendor/lge/l01f/proprietary/vendor/lib/libchromatix_imx132_common.so:system/vendor/lib/libchromatix_imx132_common.so \
     vendor/lge/l01f/proprietary/vendor/lib/libchromatix_imx132_default_video.so:system/vendor/lib/libchromatix_imx132_default_video.so \
     vendor/lge/l01f/proprietary/vendor/lib/libchromatix_imx132_mms_video.so:system/vendor/lib/libchromatix_imx132_mms_video.so \
